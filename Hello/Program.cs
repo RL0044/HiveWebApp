163edbd3a8ec0f37e90c4c13f43a216b5f45503d
RL0044 <147439580+RL0044@users.noreply.github.com>
2024-02-19 12:52:07 -0700

    added index and css file

---
+++ Hello/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    static void Main()
+    {
+        Circle circle = new Circle();
+        Circle circle2 = new Circle(1,2,3);
+        Circle circle3 = new Circle(){
+            centreX = 1, centreY = 2, radius = 3
+        };
+        circle.centreX = 1;
+        circle.centreY = 2;
+        circle.radius = 3;
+    }
+}
